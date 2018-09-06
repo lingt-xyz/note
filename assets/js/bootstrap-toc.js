778d9b4b4a2727d518f7d0004d4e129b29d2a360
Ling Tan <ta_li@encs.concordia.ca>
2018-09-05 21:26:21 -0400

    Update bootstrap-toc.js

--- assets/js/bootstrap-toc.js
+++ assets/js/bootstrap-toc.js
@@ -12,7 +12,7 @@
         // http://danielnouri.org/notes/2011/03/14/a-jquery-find-that-also-finds-the-root-element/
         // http://stackoverflow.com/a/12731439/358804
         var $descendants = $el.find(selector);
-        return $el.filter(selector).add($descendants).filter(':not([data-toc-skip])'); //":not(.data-toc-skip)"
+        return $el.filter(selector).add($descendants).filter(':not(.data-toc-skip)'); //:not([data-toc-skip])
       },
 
       generateUniqueIdBase: function(el) {
